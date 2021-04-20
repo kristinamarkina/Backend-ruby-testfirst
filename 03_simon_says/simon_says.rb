757bf428e93a0189fb023ddb61c59fb075a43b80
Kristina Markina <kr.markina@gmail.com>
2021-04-19 23:30:41 -0400

    simon-says.rb modified

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,33 @@
-#write your code here
+def echo(word)
+  word
+end
+
+def shout(word)
+  word.upcase
+end
+
+def repeat(word,n=1)
+  return word + " " + word if n == 1
+  ans = word
+  (n-1).times {ans += " " + word}
+  return ans
+end
+
+def start_of_word(word,n)
+  word[0..n-1]
+end
+
+def first_word(word)
+  first_space = word.index(" ")
+  word[0..first_space-1]
+end
+
+def titleize(string)
+  arr = string.split
+  arr[0].capitalize!
+  arr.each do |word|
+    word.capitalize! if word.length > 4
+  end
+  arr[-1].capitalize!
+  arr.join(" ")
+end
