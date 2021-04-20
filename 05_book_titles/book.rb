331439dce06c0b74b4c74006cd87ac3977e7c38f
Kristina Markina <kr.markina@gmail.com>
2021-04-20 19:06:06 -0400

    changed book.rb

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,37 @@
+ARTICLES = %w[the a an]
+CONJUNCTIONS = %w[for and nor but or yet so]
+PREPOSITIONS = %w[to from over under on in of]
+
 class Book
-# write your code here
+  attr_accessor :title
+  def initialize(title = '')
+    @title = title
+  end
+
+  def capitalize?(word)
+    if ARTICLES.include? word
+      return false
+    end
+    if  CONJUNCTIONS.include? word
+      return false
+    end
+    if  PREPOSITIONS.include? word
+      return false
+    end
+    return true
+  end
+
+  def title
+    arr = @title.split
+    arr[0].capitalize!
+    arr.each do |word|
+      if capitalize?(word) == true
+        word.capitalize!
+      end
+    end
+    arr.join(" ")
+  end
+
+
+  # write your code here
 end
