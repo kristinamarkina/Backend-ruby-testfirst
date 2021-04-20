4dd2618f811a0c00d6dc1cf9455cb0d71c2e6581
Kristina Markina <kr.markina@gmail.com>
2021-04-19 22:52:25 -0400

    modified calclator.rb and calculator_spec.rb

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,27 @@
-#write your code here
+def add(a,b)
+  a+b
+end
+
+def subtract(a,b)
+  a-b
+end
+
+def sum(arr)
+  sum = 0
+  arr.each {|a| sum += a}
+  return sum
+end
+
+def multiply(a,b)
+  a*b
+end
+
+def power(a,n)
+  a**n
+end
+
+def factorial(a)
+  f = 1
+  (1..a).each {|num| f *= num}
+  return f
+end
