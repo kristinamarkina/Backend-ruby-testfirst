0d70e7b311c5d6f1998d9b7cb309bb25a38c8b06
Kristina Markina <kr.markina@gmail.com>
2021-04-21 19:22:21 -0400

    modified timer.rb

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,36 @@
+ZERO = '00:00:00'
+
 class Timer
-  #write your code here
+  attr_accessor :seconds
+  def initialize
+    @seconds = 0
+  end
+
+  def seconds
+    @seconds
+  end
+
+  def time_string
+    temp = @seconds
+    hours = temp / 3600
+    temp -= hours * 3600
+    minutes = temp / 60
+    temp -= minutes * 60
+    if hours < 10
+      hours = '0'+hours.to_s
+    else
+      hours = hours.to_s
+    end
+    if minutes < 10
+      minutes = '0'+minutes.to_s
+    else
+      minutes = minutes.to_s
+    end
+    if temp < 10
+      temp = '0'+temp.to_s
+    else
+      temp = temp.to_s
+    end
+    hours + ':'+ minutes + ':' + temp
+  end
 end
